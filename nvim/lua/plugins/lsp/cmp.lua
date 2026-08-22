f93181e9c46d7fc1ee99f7236506cfa43dd05ba0
SIDNEYHOLMES <sidneyholmesdev@gmail.com>
2026-08-22 13:22:51 -0500

    changed preview file up/down key (up = f, down = b)

--- nvim/lua/plugins/lsp/cmp.lua
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -51,8 +51,8 @@ return {
       },
       -- Key mappings for the completion popup
       mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-b>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<C-y>"] = cmp.mapping.confirm({ select = false }),
